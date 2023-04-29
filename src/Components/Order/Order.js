94c2c2b06a805b755d5911d553ded2fbc74e9434
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-04-29 10:46:13 +0600

    total price updated

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import './Order.css' ;
+import './Order.css';
 
-const Order = () => {
+const Order = ({ cart }) => {
+
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for (const p of cart) {
+        // console.log(p.price);
+        total += p.price;
+        shipping += p.shipping;
+    }
+
+    const tax_num = total * 0.07;
+    const tax = tax_num.toFixed(2);
+
+    const grand = total + shipping + tax;
+
     return (
-        <div>
-           <h1>Order Summary </h1> 
+        <div className='cart'>
+            <h1>Order Summary </h1>
+            <p> Selected Items : {cart.length}</p>
+            <p>Product Price : $ {total} </p>
+            <p>Total Shipping : {shipping} </p>
+            <p>Tax :  {tax} </p>
+            <h4>Grand Total Price : {grand} </h4>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -6,21 +6,35 @@ import Order from '../Order/Order';
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect( ()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then (data => setProducts(data))
-    }, [])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    const [carts, setCarts] = useState([]);
+
+    const handleCart = (product) => {
+        // console.log(product.name, "added to the cart");
+        
+        const newCart = [...carts, product];
+        setCarts(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className="product-container">
-              {
-                products.map(product => <Product key = {product.id} product = {product}> </Product>)
-              }
+                {
+                    products.map(product => <Product key={product.id} product={product}
+                        handleCart={handleCart}
+                    ></Product>)
+                }
             </div>
 
             <div className="cart-container">
-                <Order></Order>
+                <Order 
+                cart = {carts}
+                ></Order>
             </div>
         </div>
     );
