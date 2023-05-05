e3949de43db3c2381ba27a00005ddcee0ebca399
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-05-05 15:11:12 +0600

    show product on ui from local storage

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -13,16 +13,42 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        const savedCart = [];
+        // step-1 : get Id from shopping cart
+        for (const id in storedCart) {
+
+            // step-2 : find the product using id
+            const addedCart = products.find(product => product.id === id);
+
+            if (addedCart) {
+
+                // step-3 : set quantity
+                const quantity = storedCart[id];
+
+                // console.log(quantity);
+
+                addedCart.quantity = quantity;
+
+                // console.log(addedCart);
+
+                // step-4 : stored in savedCart
+                savedCart.push(addedCart);
+
+            }
+
+        }
+        // step-5 : added to cart from local storage
+        setCarts(savedCart);
+    }, [products])
 
     const [carts, setCarts] = useState([]);
 
     const handleCart = (product) => {
         // console.log(product.name, "added to the cart");
-        
+
         const newCart = [...carts, product];
         setCarts(newCart);
         addToDb(product.id)
@@ -39,8 +65,8 @@ const Shop = () => {
             </div>
 
             <div className="cart-container">
-                <Order 
-                cart = {carts}
+                <Order
+                    cart={carts}
                 ></Order>
             </div>
         </div>
